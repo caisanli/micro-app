94dc94fbecb3053d8098c078bdadf5c0a2bf2733
CaisanLi <caisanli_0105@163.com>
2021-09-05 22:55:47 +0800

    fix：调整代码

--- src/sandbox/diff.js
+++ src/sandbox/diff.js
@@ -11,16 +11,16 @@ class DiffSandbox {
     for (const item in window) {
       this.windowSnapshot[item] = window[item]
     }
-    Object.keys(this.modifyMap).forEach((p) => {
-      window[p] = this.modifyMap[p]
-    })
+    // Object.keys(this.modifyMap).forEach((p) => {
+    //   window[p] = this.modifyMap[p]
+    // })
   }
   stop() {
     // 还原window的属性
     for (const item in window) {
       if (this.windowSnapshot[item] !== window[item]) {
         // 记录变更
-        this.modifyMap[item] = window[item]
+        // this.modifyMap[item] = window[item]
         // 还原window
         window[item] = this.windowSnapshot[item]
       }
