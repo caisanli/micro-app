eb9d343262f5fe5e0e1a9e66e71d6bfed4929c73
caigx <caisanli_0105@163.com>
2023-06-08 19:08:06 +0800

    feat(index)：移除vite插件

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@
 import MicroApp from './micro';
 import setPreload from './utils/preload';
 import appEvent, { baseAppEvent } from './sandbox/event';
-import { vitePlugin as microAppVitePlugin } from './plugin/vite';
+// import { vitePlugin as microAppVitePlugin } from './plugin/vite';
 
 export {
   setPreload,
   baseAppEvent,
   appEvent,
   MicroApp,
-  microAppVitePlugin
+  // microAppVitePlugin
 };
 
 export default MicroApp;
--- src/plugin/vite.ts
+++ src/plugin/vite.ts
@@ -1,36 +1,36 @@
-import { join } from 'path';
-import { writeFileSync } from 'fs';
-import type { Plugin } from 'vite';
-import type { OutputChunk } from 'rollup';
-
-const vitePlugin = (): Plugin => {
-  let basePath = '';
-  return {
-    name: 'vite:micro-app',
-    apply: 'build',
-    configResolved(config) {
-      basePath = `${ config.base }${ config.build.assetsDir }/`;
-    },
-    writeBundle(options, bundle) {
-      for (const chunkName in bundle) {
-        if (Object.prototype.hasOwnProperty.call(bundle, chunkName)) {
-          const chunk = bundle[chunkName] as OutputChunk;
-          if (chunk.fileName && chunk.fileName.lastIndexOf('.js') > -1) {
-            const ORIGIN = 'http://192.168.0.103:83';
-            chunk.code = chunk.code.replace(/(from|import\()(\s*['"])(\.\.?\/)/g, (all, $1, $2, $3) => {
-              const fullPath = new URL($3, ORIGIN + basePath);
-              const newPath = fullPath.href.replace(ORIGIN, '');
-              return all.replace($3, newPath);
-            });
-            const fullPath = join(options.dir || '', chunk.fileName);
-            writeFileSync(fullPath, chunk.code);
-          }
-        }
-      }
-    },
-  };
-};
-
-export {
-  vitePlugin
-};
+// import { join } from 'path';
+// import { writeFileSync } from 'fs';
+// import type { Plugin } from 'vite';
+// import type { OutputChunk } from 'rollup';
+//
+// const vitePlugin = (): Plugin => {
+//   let basePath = '';
+//   return {
+//     name: 'vite:micro-app',
+//     apply: 'build',
+//     configResolved(config) {
+//       basePath = `${ config.base }${ config.build.assetsDir }/`;
+//     },
+//     writeBundle(options, bundle) {
+//       for (const chunkName in bundle) {
+//         if (Object.prototype.hasOwnProperty.call(bundle, chunkName)) {
+//           const chunk = bundle[chunkName] as OutputChunk;
+//           if (chunk.fileName && chunk.fileName.lastIndexOf('.js') > -1) {
+//             const ORIGIN = 'http://192.168.0.103:83';
+//             chunk.code = chunk.code.replace(/(from|import\()(\s*['"])(\.\.?\/)/g, (all, $1, $2, $3) => {
+//               const fullPath = new URL($3, ORIGIN + basePath);
+//               const newPath = fullPath.href.replace(ORIGIN, '');
+//               return all.replace($3, newPath);
+//             });
+//             const fullPath = join(options.dir || '', chunk.fileName);
+//             writeFileSync(fullPath, chunk.code);
+//           }
+//         }
+//       }
+//     },
+//   };
+// };
+//
+// export {
+//   vitePlugin
+// };
