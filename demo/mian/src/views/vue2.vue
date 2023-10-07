19a599678e401420da9cca7e7f6be2aa60171ded
caigx <caisanli_0105@163.com>
2023-10-07 15:12:19 +0800

    feat(demo)：test

--- demo/mian/src/views/vue2.vue
+++ demo/mian/src/views/vue2.vue
@@ -1,16 +1,17 @@
 /* eslint-disable */
 <template>
-    <micro-app name="vue2" url="/vue2" />
+    <micro-app name="vue2" url="/vue2" styleSandbox="true" sandbox="false" />
 </template>
 
 <script>
-    /* eslint-disable */
-    import MicroApp from '../../../../src/index';
-    export default {
-        components: {
-            MicroApp
-        }
-    }
+// /* eslint-disable */
+// import MicroApp from '../../../../src/index';
+// // import { ref } from 'vue';
+// export default {
+//   components: {
+//     MicroApp,
+//   }
+// }
 </script>
 
 <style lang="less" scoped>
